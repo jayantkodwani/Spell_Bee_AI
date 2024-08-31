03a2f5f3591798634ea0fd9fc0d568f4589776d1
Jayant Kodwani <JAYANT.KODWANI@YAHOO.IN>
2024-08-31 20:30:33 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, jsonify
 import random
 
-from flask_talisman import Talisman
-Talisman()
-
-
 app = Flask(__name__)
 
 # List of words for the spelling bee practice
