1e96269ffe9cf876f30f875e8b8dac55fcebf467
Jayant Kodwani <JAYANT.KODWANI@YAHOO.IN>
2024-08-31 19:11:36 +0400

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def get_word():
 
 #if __name__ == '__main__':
     # Bind to all interfaces (0.0.0.0) and use the port defined in the environment variable, default to 8000
-#    import os
+    import os
 #    port = int(os.environ.get('PORT', 8000))
  #   app.run(host='0.0.0.0', port=port)
+app.run()
